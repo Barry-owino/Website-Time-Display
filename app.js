16186deb976d8dfc4c05633df5abe387e18578d0
Barry-owino <barryowino5@gmail.com>
2022-06-21 13:00:41 +0300

    added the settime interval

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const time = document.getElementById("current-time");
 
-let d = new Date();
-time.innerHTML = d.toLocaleTimeString();
+setInterval(() => {
+  let d = new Date();
+  time.innerHTML = d.toLocaleTimeString();
+})
